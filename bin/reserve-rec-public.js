d3ccaeec7cc551265052e90ec9ac26614a77735f
cameron <cameron@digitalspace.ca>
2025-02-20 18:51:56 +0000

    temp commit

---
+++ bin/reserve-rec-public.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const cdk = require('aws-cdk-lib');
+const { ReserveRecPublicStack } = require('../lib/reserve-rec-public-stack');
+
+const app = new cdk.App();
+new ReserveRecPublicStack(app, 'ReserveRecPublicStack', {
+  env: {
+    //AWS account variables
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+
+    // Custom environment variables
+    ENVIRONMENT: process.env.ENVIRONMENT || 'dev',
+    S3_BUCKET_ADMIN: process.env.S3_BUCKET_PUBLIC || 'reserve-rec-public-cdk',
+    API_STAGE: process.env.API_STAGE || 'api',
+  }
+});

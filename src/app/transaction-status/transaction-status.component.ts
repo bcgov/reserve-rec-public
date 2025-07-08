5c80374796b812b006b1f77981a6052cf3f17af4
David <daveclaveau@gmail.com>
2025-07-08 10:38:07 -0700

    PRDT-101: sendback fixes
    Signed-off-by: David <daveclaveau@gmail.com>

--- src/app/transaction-status/transaction-status.component.ts
+++ src/app/transaction-status/transaction-status.component.ts
@@ -7,15 +7,6 @@ import { LoadingService } from '../services/loading.service';
 import { Constants } from '../constants';
 import { BookingService } from '../services/booking.service';
 
-const TRANSACTION_PARAMS = [
-  'trnId', 'messageId', 'messageText', 'authCode', 'responseType',
-  'trnAmount', 'trnDate', 'trnOrderNumber', 'trnLanguage',
-  'trnCustomerName', 'trnEmailAddress', 'trnPhoneNumber', 'avsProcessed',
-  'avsId', 'avsResult', 'avsAddrMatch', 'avsPostalMatch', 'avsMessage',
-  'cvdId', 'cardType', 'trnType', 'paymentMethod',
-  'ref1', 'ref2', 'ref3', 'ref4', 'ref5', 'hashValue'
-];
-
 @Component({
   selector: 'app-transaction-status',
   imports: [CommonModule],
@@ -34,10 +25,10 @@ export class TransactionStatusComponent {
     protected bookingService: BookingService,
     protected loadingService: LoadingService
   ) {
-    this.route.queryParamMap.subscribe((params) => {
-      const queryParams = this.parseQueryParams(params);
-      this.queryTransaction(queryParams);
-    });
+    const params = this.route.snapshot.queryParams;
+    const clientTransactionId = params['trnOrderNumber'];
+    const sessionId = params['ref2'];
+    this.queryTransaction(clientTransactionId, sessionId);
   }
 
   transactionEffect = effect(() => {
@@ -46,35 +37,29 @@ export class TransactionStatusComponent {
       this.queryResponse = response;
       this.lastProcessedResponse = response;
 
-      const transactionStatus = response?.transaction?.data?.status;
-      const bookingStatus = response?.booking?.data?.bookingStatus;
-      
       // If transaction comes back as paid and the booking is complete, continue
-      if (transactionStatus === 'paid' && bookingStatus === 'completed') {
-        this.router.navigate(['/booking-confirmation', response?.transaction?.data?.ref1]);
+      if (response?.trnApproved == 1) {
+        this.router.navigate(['/booking-confirmation', response?.ref1]);
       } else {
-        // TODO: if fails, redirect to checkout with sessionId
-        // this.router.navigate(['/checkout'], { queryParams: { id: response?.transaction?.data?.ref2 } });
+        // TODO: if fails, redirect to checkout with sessionIds
         console.log('Redirecting to home...');
         this.router.navigate(['/']);
       }
     }
   });
 
-  private parseQueryParams(paramMap): Record<string, string | null> {
-    return Object.fromEntries(
-      TRANSACTION_PARAMS.map((param) => [param, paramMap.get(param)])
-    );
-  }
-
-  async queryTransaction(queryParams: Record<string, string | null>) {
+  async queryTransaction(clientTransactionId: string, sessionId: string) {
     try {
       this.loadingService.addToFetchList(Constants.dataIds.TRANSACTION_STATUS_RESULTS);
+      const queryParams = {
+        clientTransactionId,
+        sessionId
+      };
       const res: any = await lastValueFrom(
-        this.apiService.put(`transactions`, {}, queryParams)
+        this.apiService.get(`transactions`, queryParams)
       );
       console.log('Transaction res:', res);
-      this._queryResponseSignal.set(res.data?.response);
+      this._queryResponseSignal.set(res?.data);
     } catch (error) {
       console.error('Failed to query transaction details:', error);
     } finally {

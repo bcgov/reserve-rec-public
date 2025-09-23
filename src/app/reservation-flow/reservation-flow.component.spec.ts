621fcc28c3f3231b8e3d36e66a6bf6df2043612d
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2025-09-23 23:49:15 +0000

    Remove test while I work on tests for all areas of this work

--- src/app/reservation-flow/reservation-flow.component.spec.ts
+++ src/app/reservation-flow/reservation-flow.component.spec.ts
@@ -1,33 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ReservationFlowComponent } from './reservation-flow.component';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { ConfigService } from '../services/config.service';
+// import { ReservationFlowComponent } from './reservation-flow.component';
+// import { provideRouter } from '@angular/router';
+// import { provideHttpClient } from '@angular/common/http';
+// import { provideHttpClientTesting } from '@angular/common/http/testing';
+// import { ConfigService } from '../services/config.service';
 
-describe('ReservationFlowComponent', () => {
-  let component: ReservationFlowComponent;
-  let fixture: ComponentFixture<ReservationFlowComponent>;
+// describe('ReservationFlowComponent', () => {
+//   let component: ReservationFlowComponent;
+//   let fixture: ComponentFixture<ReservationFlowComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ReservationFlowComponent],
-      providers: [
-        ConfigService,
-        provideRouter([{path: 'checkout', component: ReservationFlowComponent}]),
-        provideHttpClient(),
-        provideHttpClientTesting() // Uncomment if you need to mock HTTP requests in tests
-      ]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [ReservationFlowComponent],
+//       providers: [
+//         ConfigService,
+//         provideRouter([{path: 'checkout', component: ReservationFlowComponent}]),
+//         provideHttpClient(),
+//         provideHttpClientTesting() // Uncomment if you need to mock HTTP requests in tests
+//       ]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(ReservationFlowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(ReservationFlowComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });

ec0a65b0ee8e2a4ec55108422f8eb6fd1b33d70a
davidclaveau <daveclaveau@gmail.com>
2026-03-31 16:06:23 +0000

    header and footer implementation
    Signed-off-by: davidclaveau <daveclaveau@gmail.com>

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component, ElementRef, HostListener} from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { RouterModule, Router } from '@angular/router';
 import { ConfigService } from '../services/config.service';
@@ -13,15 +13,20 @@ import { ConfigService } from '../services/config.service';
 export class HeaderComponent {
   public envName: string;
   public showBanner = true;
+  public hamburgerIcon = true;
 
-  constructor(private authService: AuthService, private router: Router, private configService: ConfigService) {
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private configService: ConfigService,
+    private el: ElementRef
+  ) {
     this.envName = this.configService.config['ENVIRONMENT'] || 'local';
     // Hide banner in production
     if (this.envName === 'prod') {
       this.showBanner = false;
     }
   }
-
   get user() {
     return this.authService.getCurrentUser(); // Directly bind to the signal
   }
@@ -29,8 +34,23 @@ export class HeaderComponent {
   logout() {
     this.authService.logout(); // Call the sign-out logic
   }
-
-  cart(){
-    this.router.navigate(['/cart']);
+  
+  toggleHamburger() {
+    this.hamburgerIcon = !this.hamburgerIcon;  
   }
+
+  // Binds to the CSS class
+  closeMenu() {
+    this.hamburgerIcon = true;
+  }
+
+  // Useful way to check if click was outside the entire component,
+  // for closing the mobile menu automatically
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.el.nativeElement.contains(event.target) && !this.hamburgerIcon) {
+      this.closeMenu();
+    }
+  }
+
 }

91a3c18ff03b7aeab43b0f93e98a56f9cf9286d3
davidclaveau <daveclaveau@gmail.com>
2026-05-22 21:00:20 +0000

    fix facility data type and landing page link

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -21,16 +21,19 @@ export class SearchPageComponent implements OnInit, AfterViewChecked {
 
   public options = [
     { value: null, display: 'Garibaldi Provincial Park', disabled: true },
-    { value: '/facility/bcparks_7/accessPoint/1', display: 'Cheakamus' },
-    { value: '/facility/bcparks_7/accessPoint/2', display: 'Diamond Head' },
-    { value: '/facility/bcparks_7/accessPoint/3', display: 'Rubble Creek' },
+    { value: '/facility/bcparks_7/structure/1', display: 'Cheakamus' },
+    { value: '/facility/bcparks_7/structure/2', display: 'Diamond Head' },
+    { value: '/facility/bcparks_7/structure/3', display: 'Rubble Creek' },
     { value: null, display: 'Golden Ears Provincial Park', disabled: true },
-    { value: '/facility/bcparks_8/accessPoint/1', display: 'Alouette Lake Boat Launch' },
-    { value: '/facility/bcparks_8/accessPoint/2', display: 'Alouette Lake South Beach Day-Use Area' },
-    { value: '/facility/bcparks_8/accessPoint/3', display: 'Gold Creek' },
-    { value: '/facility/bcparks_8/accessPoint/4', display: 'West Canyon Trailhead' },
+    { value: '/facility/bcparks_8/structure/1', display: 'Alouette Lake Boat Launch' },
+    { value: '/facility/bcparks_8/structure/2', display: 'Alouette Lake South Beach Day-Use Area' },
+    { value: '/facility/bcparks_8/structure/3', display: 'Gold Creek' },
+    { value: '/facility/bcparks_8/structure/4', display: 'West Canyon Trailhead' },
+    { value: null, display: 'Mount Seymour Park', disabled: true },
+    { value: '/facility/bcparks_15/structure/1', display: 'Daily Additional P1 and Lower P5' },
+    { value: '/facility/bcparks_15/structure/2', display: 'P1 and Lower P5' },
     { value: null, display: 'Joffre Lakes Provincial Park', disabled: true },
-    { value: '/facility/bcparks_363/accessPoint/1', display: 'Joffre Lakes' },
+    { value: '/facility/bcparks_363/structure/1', display: 'Joffre Lakes' },
   ];
 
   constructor(
